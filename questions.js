70a2cbf7b9c4e05a80fe8e59c8993de5b4502d7c
akncyunus-lab <akncyunus@gmail.com>
2026-08-15 10:29:58 +0300

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,42 @@
+const questions = [
+
+  {
+    question: "12 + 8 işleminin sonucu kaçtır?",
+    options: [
+      "18",
+      "20",
+      "22",
+      "24"
+    ],
+    answer: 1,
+    level: "Ortaöğretim",
+    lesson: "Matematik"
+  },
+
+  {
+    question: "İlk Türk devletlerinde hükümdarın ülkeyi yönetme yetkisinin Tanrı tarafından verildiğine inanılan anlayış hangisidir?",
+    options: [
+      "Kut anlayışı",
+      "Töre",
+      "Kurultay",
+      "İkili teşkilat"
+    ],
+    answer: 0,
+    level: "Ortaöğretim",
+    lesson: "Tarih"
+  },
+
+  {
+    question: "Aşağıdaki cümlelerin hangisinde neden-sonuç ilişkisi vardır?",
+    options: [
+      "Bugün erkenden yola çıktı.",
+      "Yağmur yağdığı için maç ertelendi.",
+      "Kitabını masanın üzerine bıraktı.",
+      "Akşam arkadaşlarıyla buluştu."
+    ],
+    answer: 1,
+    level: "Ortaöğretim",
+    lesson: "Türkçe"
+  }
+
+];
